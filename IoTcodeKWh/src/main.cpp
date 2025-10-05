99e5c117fbc48dcf8b676c2f304190addf72b755
Marco <marco.corral@usach.cl>
2025-10-05 10:49:23 -0300

    code base sensor corriente

--- IoTcodeKWh/src/main.cpp
+++ IoTcodeKWh/src/main.cpp
@@ -1,18 +1,42 @@
-#include <Arduino.h>
+// Sensibilidad del sensor en V/A
+float SENSIBILITY = 0.185;   // Modelo 5A
+//float SENSIBILITY = 0.100; // Modelo 20A
+//float SENSIBILITY = 0.066; // Modelo 30A
 
-// put function declarations here:
-int myFunction(int, int);
+int SAMPLESNUMBER = 100;
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+void setup() 
+{
+  Serial.begin(9600);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void printMeasure(String prefix, float value, String postfix)
+{
+  Serial.print(prefix);
+  Serial.print(value, 3);
+  Serial.println(postfix);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void loop()
+{
+  float current = getCorriente(SAMPLESNUMBER);
+  float currentRMS = 0.707 * current;
+  float power = 230.0 * currentRMS;
+
+  printMeasure("Intensidad: ", current, "A ,");
+  printMeasure("Irms: ", currentRMS, "A ,");
+  printMeasure("Potencia: ", power, "W");
+  delay(1000);
+}
+
+float getCorriente(int samplesNumber)
+{
+  float voltage;
+  float corrienteSum = 0;
+  for (int i = 0; i < samplesNumber; i++)
+  {
+    voltage = analogRead(A0) * 5.0 / 1023.0;
+    corrienteSum += (voltage - 2.5) / SENSIBILITY;
+  }
+  return(corrienteSum / samplesNumber);
 }
